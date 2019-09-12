0c5b1ce9fcdf9151bfcbb0d8bad5839b0f824129
nbignell2 <46579584+nbignell2@users.noreply.github.com>
2019-09-12 13:42:53 -0600

    CRUD

--- src/Code-First from Database/WestWindSystem/DAL/WestWindContext.cs
+++ src/Code-First from Database/WestWindSystem/DAL/WestWindContext.cs
@@ -4,6 +4,7 @@ namespace WestWindSystem.Entities
     using System.Data.Entity;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
+    using WestWindSystem.Entities;
 
     public partial class WestWindContext : DbContext
     {
