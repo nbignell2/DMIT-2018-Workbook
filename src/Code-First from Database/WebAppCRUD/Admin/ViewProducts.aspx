de8ebafd5e24b2b4dc05718a3ff371a85f784c5c
nbignell2 <46579584+nbignell2@users.noreply.github.com>
2019-09-12 14:10:55 -0600

    BLL

---
+++ src/Code-First from Database/WebAppCRUD/Admin/ViewProducts.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ViewProducts.aspx.cs" Inherits="WebAppCRUD.Admin.ViewProducts" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1 class="page-header">View Products</h1>
+
+</asp:Content>
---
+++ src/Code-First from Database/WestWindSystem/BLL/CRUDController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+    public class CRUDController
+    {
+        #region Products CRUD
+        public List<Product> ListProducts()
+        {
+            return null;
+        }
+        #endregion
+    }
+}
