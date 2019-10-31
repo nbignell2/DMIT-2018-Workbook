6aa9628aae464c623bdc3a1728313712c57b86aa
nbignell2 <46579584+nbignell2@users.noreply.github.com>
2019-10-31 13:39:54 -0600

    All the things

---
+++ src/Custom Identity/WebApp/Admin/Security/SecurityDbContextInitializer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+
+// You can learn about Database Initialization Strategies in EF6 via
+// http://www.entityframeworktutorial.net/code-first/database-initialization-strategy-in-code-first.aspx
+
+namespace WebApp.Admin.Security
+{
+    /// <summary>
+    /// Provide functionality for setting up the database for the ApplicationDbContext.
+    /// The specific functionality is to create the database if it does not exist.
+    /// </summary>
+    public class SecurityDbContextInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //To "seed" a database is to provide it with some initial data
+            // when the databaase is created
+
+            #region Seed the security roles
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            //The RoleManager<T> and RoleStore<T> are BLL classes that give flexibility 
+            // to the design/structure of how we're using Asp.Net identity.
+            // The identityrole is an entity class that represents a security role
+
+
+            // Hard coded security roles (move later on)
+            roleManager.Create(new IdentityRole { Name = "Administrators" });
+            roleManager.Create(new IdentityRole { Name = "Registered Users" });
+
+            #endregion
+
+
+            #region Seed the users
+            #endregion
+
+            //Keep the call to the base class to do its seeding work
+            base.Seed(context);
+        }
+    }
+}
